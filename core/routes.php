ea25c078723d7b7c172cfbb2bc5228be81412e27
unknown <ap.castrocastellon@gmail.com>
2024-05-02 20:52:54 -0400

    ajusta archivo de ruteo a solo identificar acciones

--- core/routes.php
+++ core/routes.php
@@ -1,26 +1,23 @@
 <?php
-	
-	function cargarControlador($controlador){		
-		$nombreControlador = ucwords($controlador)."Controller";
-		$urlArchivoControlador = 'controllers/'.ucwords($controlador).'.php';
-		
-		if(!is_file($urlArchivoControlador)){			
-			$urlArchivoControlador= 'controllers/'.CONTROLADOR_PRINCIPAL.'.php';
-		}
-		require_once $urlArchivoControlador;
-		$control = new $nombreControlador();
-		return $control;
+
+function cargarControlador($controlador)
+{
+	$nombreControlador = ucwords($controlador) . "Controller";
+	$urlArchivoControlador = 'controllers/' . ucwords($controlador) . '.php';
+
+	if (!is_file($urlArchivoControlador)) {
+		$urlArchivoControlador = 'controllers/' . CONTROLADOR_PRINCIPAL . '.php';
 	}
-	
-	function cargarAccion($controller, $accion, $id = null){
-		if(isset($accion) && method_exists($controller, $accion)){
-			if($id == null){
-				$controller->$accion();
-				} else {
-				$controller->$accion($id);
-			}
-			} else {
-			$controller->ACCION_PRINCIPAL();
-		}	
+	require_once $urlArchivoControlador;
+	$control = new $nombreControlador();
+	return $control;
+}
+
+function cargarAccion($controller, $accion)
+{
+	if (isset($accion) && method_exists($controller, $accion)) {
+		$controller->$accion();
+	} else {
+		$controller->ACCION_PRINCIPAL();
 	}
-?>
+}
